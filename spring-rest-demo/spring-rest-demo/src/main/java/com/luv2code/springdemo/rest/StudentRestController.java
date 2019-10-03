0c5f493d2d639fa5f6d638f6d6407c5e8dbdd78a
othnvun <naveen.vundavalli@medavie.bluecross.ca>
2019-10-03 16:15:13 -0300

    ofc

--- spring-rest-demo/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,13 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +19,52 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
+	
+	private List<Student> theStudents;
+	
+	
+	//define @PostConstruct to load the student data only once
+	@PostConstruct
+	public void loadData(){
+		theStudents=new ArrayList<>();
+		
+		theStudents.add(new Student("sai","ram"));
+		theStudents.add(new Student("sai","krishna"));
+		theStudents.add(new Student("sai","murali"));
+		
+	}
+	
+	
 	@GetMapping("/students")
 	//end point retuns list of students
 	public List<Student> getStudents(){
 		
-		List<Student> thestudents=new ArrayList<>();
 		
-		thestudents.add(new Student("sai","ram"));
-		thestudents.add(new Student("sai","krishna"));
-		thestudents.add(new Student("sai","murali"));
-		
-		return thestudents;
+		return theStudents;
 	}
+	
+	
+	//define endpoint for "/students/${studentID}"  retunr student index
+	
+	@GetMapping("/students/{studentId}")
+	//end point retuns list of students
+	public Student getStudent(@PathVariable int studentId){
+		
+		//check if student is present or not .if not throw new exception throeu the class studnetnotfoundexception we have created
+		if(studentId>=theStudents.size()||studentId<0){
+			throw new StudentNotFoundException("Student id not found :"+studentId);
+		}
+		
+		Student temp=theStudents.get(studentId);
+		
+		return temp ;
+	}
+	
+	
+	//Add an exceptoin handler using @ExceptionHandler
+	
+	
+	
+	
+	
 }
--- spring-rest-demo/spring-rest-demo/src/main/webapp/index.jsp
+++ spring-rest-demo/spring-rest-demo/src/main/webapp/index.jsp
@@ -13,6 +13,8 @@ SPRING DEMO
  
  <a href="${pageContext.request.contextPath}/test/hello">Hello</a>
  
+<br>
+ 
  <a href="${pageContext.request.contextPath}/api/students">Get All Students</a>
   
 </body>
